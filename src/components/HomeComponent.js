33e6a30e72d109aad6744c9813b320411ba97c97
MidZM <weston.meade1@gmail.com>
2020-10-02 13:51:33 -0400

    Fetch Handling Errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import { Loading } from './LoadingComponent'
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCard({item, isLoading, errMess}) {
     if (isLoading) return <Loading />
     if (errMess) return <h4>{errMess}</h4>
     return (
         <Card>
-            <CardImg src={item.image} alt={item.name} />
+            <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.title}</CardTitle>
                 <CardText>{item.description}</CardText>
@@ -30,8 +31,8 @@ function Home(props) {
                 <div className="col-md m-1">
                     <RenderCard
                         item={props.promotion}
-                        isLoading={props.campsitesIsLoading}
-                        errMess={props.campsitesErrMess}
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
                     />
                 </div>
                 <div className="col-md m-1">
