85f6081c1c09126399035fd0c82c961986031336
MidZM <weston.meade1@gmail.com>
2020-10-03 10:56:37 -0400

    Update

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,18 +2,27 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
 function RenderCard({item, isLoading, errMess}) {
     if (isLoading) return <Loading />
     if (errMess) return <h4>{errMess}</h4>
     return (
-        <Card>
-            <CardImg src={baseUrl + item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.title}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform
+            in
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(50%)'
+            }}
+        >
+            <Card>
+                {console.log(item)}
+                <CardImg src={baseUrl + item.image} alt={item.name} />
+                <CardBody>
+                    <CardTitle>{item.title}</CardTitle>
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     );
 }
 
@@ -24,7 +33,7 @@ function Home(props) {
                 <div className="col-md m-1">
                     <RenderCard
                         item={props.campsites}
-                        isLoading={props.campsitesIsLoading}
+                        isLoading={props.campsitesLoading}
                         errMess={props.campsitesErrMess}
                     />
                 </div>
@@ -38,7 +47,7 @@ function Home(props) {
                 <div className="col-md m-1">
                     <RenderCard
                         item={props.partner}
-                        isLoading={props.campsitesIsLoading}
+                        isLoading={props.campsitesLoading}
                         errMess={props.campsitesErrMess}
                     />
                 </div>
